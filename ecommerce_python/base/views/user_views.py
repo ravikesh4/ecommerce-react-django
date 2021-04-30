3f0fe67b9bcc5430f989772267ec0d40d6b43c26
Ravikesh Yadav <ravikesh@netzary.com>
2021-04-30 23:57:51 +0530

    done

--- ecommerce_python/base/views/user_views.py
+++ ecommerce_python/base/views/user_views.py
@@ -10,7 +10,7 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from django.contrib.auth.hashers import make_password
-from rest_framework import status   
+from rest_framework import status
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
